4d4845efd4d67fb6996e031e7d995f10987a6587
dave-tn <sr@tinynimbus.com>
2021-04-24 15:35:22 +0100

    little helper to convert nation slugs to user friendly strings

---
+++ src/utils/tankNations.tsx
@@ -0,0 +1,16 @@
+
+/**
+ * Note: TODO: Probably better this nations list is automated based on data from WG, that way we don't need to manually update if/when new nations are updated
+ */
+
+const NATIONS: { [key: string]: string } = {
+    usa: 'U.S.A.', germany: 'Germany', uk: 'U.K.', ussr: 'U.S.S.R.', france: 'France', japan: 'Japan', china: 'China', czech: 'Czechoslovakia', sweden: 'Sweden', poland: 'Poland', italy: 'Italy', merc: 'Mercenaries'
+}
+
+function toNationString(slug: string): string {
+    return NATIONS[slug] ?? 'unknown nation'
+}
+
+export {
+    toNationString
+}
