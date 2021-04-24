8092ae0d9055864c1d0757c75586e727530cb126
dave-tn <sr@tinynimbus.com>
2021-04-24 16:39:06 +0100

    initial header row, tank basic info/overview (logo, name, type, nation, tier)

---
+++ src/RowComponents/HeaderRow.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react'
+
+import styles from './HeaderRow.module.css'
+
+import { TankConfig } from '../TanksTable'
+import { toRoman } from '../utils/tankTiers'
+import { toNation } from './../utils/tankNations'
+import { toType } from './../utils/tankTypes'
+
+const HeaderRow:FC<{ data: TankConfig[] }> = ({ data }) => {
+
+    return (
+        <tr>
+            <td></td> { /* No title/header for the row */ }
+            { data.map(td => {
+                if (!td.rawData) return <td>Missing tank data<br/> for this config</td>
+
+                return (
+                    <td>
+                        <div className={styles.cell}>
+
+                            <img
+                                className={styles.tankImage}
+                                src={td.rawData.info.image_preview_url}
+                                alt={`${td.rawData.info.user_string} preview`}
+                            />
+                        
+                            <div className={styles.textWrap}>
+                                <span className={styles.name}>{ td.rawData.info.user_string }</span>
+                                <span>{ toType(td.rawData.info.type_slug) }</span>
+                                <span>{ toNation(td.rawData.info.nation) } | { toRoman(td.rawData.info.level) }</span>
+
+                                
+                            </div>
+
+                        </div>
+                    </td>
+                )
+            })}
+    </tr>
+    )
+
+}
+export { HeaderRow }
