99bb814b482bb8050ee2d5b19530baee3875116e
dave-tn <sr@tinynimbus.com>
2021-05-04 00:54:31 +0100

    support rows of multiple cells if array of values on provided prop paths

--- src/RowComponents/_MakeRowFromProperty.tsx
+++ src/RowComponents/_MakeRowFromProperty.tsx
@@ -1,38 +1,65 @@
 import { FC } from 'react'
 
+import styles from './RowComponentStyles.module.css'
 import { MakeCell } from './_MakeCell'
 
 
 interface MakeRow {
     uid: number
-    [key: string]: number | undefined
+    [key: string]: number | (number|undefined)[] | undefined
 }
 
-const MakeRowFromProperty:FC<{
+const MakeRowFromProperty: FC<{
     title: string
     data: MakeRow[]
     para: string
     suffix?: string
     roundTo?: number
     biggerIsBetter?: boolean
-}> = ({ title, data, para, roundTo, suffix, biggerIsBetter }) => (
-    <tr>
-        <td>{ title }</td>{ /* Title/header for the row */ }
-        { data.map(f => (
-            <td
-                key={f.uid}
-            >
-                <MakeCell
-                    val={f[para]}
-                    compVal={data[0][para]}
-                    suffix={suffix}
-                    roundTo={roundTo}
-                    biggerIsBetter={biggerIsBetter}
-                />
-            </td>
-        ))}
-    </tr>
-)
+}> = ({ title, data, para, roundTo, suffix, biggerIsBetter }) => {
+
+    return (
+        <tr>
+            <td className={styles.cellRowsTitle}>{title}</td>
+            { data.map(f => {
+
+                if (f[para] === undefined) return <td key={f.uid}>🤷‍♂️</td>
+
+                /**
+                 * TODO: FIXME: Is this really the way to do this in TS?...
+                 */
+                let cellPrimaryData: number[] = []
+                let cellCompData: number[] = []
+                if (typeof f[para] === 'number') {
+                    cellPrimaryData = [ f[para] as number ]
+                    cellCompData = [ data[0][para] as number ]
+                } else if (Array.isArray(f[para])) {
+                    cellPrimaryData = f[para] as number[]
+                    if (Array.isArray(data[0][para])) cellCompData = data[0][para] as number[]
+                }
+
+                return (
+                    <td key={f.uid}>
+                        <div className={styles.cellWrap}>
+                        { cellPrimaryData.map((val, idx) => (
+                                <MakeCell
+                                    key={`${f.uid}${idx}`}
+                                    val={val}
+                                    compVal={cellCompData[idx]}
+                                    suffix={suffix}
+                                    roundTo={roundTo}
+                                    biggerIsBetter={biggerIsBetter}
+                                />
+                            )
+                        )}
+                        </div>
+                    </td>
+                )
+
+            })}
+        </tr>
+    )
+}
 
 export {
     MakeRowFromProperty
