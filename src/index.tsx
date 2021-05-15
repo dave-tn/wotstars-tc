aa05f6decb6a8098af53ee75d4c6d6e61197de3f
dave-tn <sr@tinynimbus.com>
2021-05-15 18:50:13 +0100

    initial graphql requirements

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,34 @@ import './index.css'
 import { App } from './App'
 import reportWebVitals from './reportWebVitals'
 
+
+import { ApolloProvider } from '@apollo/client/react'
+
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+// const client = new ApolloClient({ uri, cache });
+const client = new ApolloClient({
+  uri: 'http://localhost:4000', // for now...
+  cache: new InMemoryCache()
+})
+
+// client.query({
+//   query: gql`
+//     query GetTanks {
+//       tanks {
+//         id
+//         user_string
+//       }
+//     }
+//   `
+// })
+// .then(result => console.log(result))
+
 ReactDOM.render(
   <React.StrictMode>
-      <App />
+    <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
