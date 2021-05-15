3954a8a803166f27b6d260be2f37e2eea61b21be
dave-tn <sr@tinynimbus.com>
2021-05-15 18:14:54 +0100

    support up to tier 13

--- src/utils/tankTiers.tsx
+++ src/utils/tankTiers.tsx
@@ -7,7 +7,7 @@ type RomanTuple = [
 ]
 
 const ROMAN_LEGEND: RomanTuple[] = [
-    [1, 'I'], [2, 'II'], [3, 'III'], [4, 'IV'], [5, 'V'], [6, 'VI'], [7, 'VII'], [8, 'VIII'], [9, 'IX'], [10, 'X']
+    [1, 'I'], [2, 'II'], [3, 'III'], [4, 'IV'], [5, 'V'], [6, 'VI'], [7, 'VII'], [8, 'VIII'], [9, 'IX'], [10, 'X'], [11, 'XI'], [12, 'XII'], [13, 'XIII']
 ]
 
 function toRoman(x: number): string {
