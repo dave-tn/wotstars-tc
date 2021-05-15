40021b3b374ea62a9c6941f20688253b067364ff
dave-tn <sr@tinynimbus.com>
2021-05-15 18:51:19 +0100

    erm this was this initial blah...

--- src/index.tsx
+++ src/index.tsx
@@ -4,28 +4,13 @@ import './index.css'
 import { App } from './App'
 import reportWebVitals from './reportWebVitals'
 
-
 import { ApolloProvider } from '@apollo/client/react'
-
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-// const client = new ApolloClient({ uri, cache });
 const client = new ApolloClient({
   uri: 'http://localhost:4000', // for now...
   cache: new InMemoryCache()
 })
-
-// client.query({
-//   query: gql`
-//     query GetTanks {
-//       tanks {
-//         id
-//         user_string
-//       }
-//     }
-//   `
-// })
-// .then(result => console.log(result))
 
 ReactDOM.render(
   <React.StrictMode>
