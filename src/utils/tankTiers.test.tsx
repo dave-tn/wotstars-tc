c379bee40ae450b28dbc91d7517cb9061e751914
dave-tn <sr@tinynimbus.com>
2021-05-17 20:42:28 +0100

    simplify converting roman numerals from arabic numbers and add some basic tests

---
+++ src/utils/tankTiers.test.tsx
@@ -0,0 +1,21 @@
+import { toRoman, KNOWN_TIERS } from './tankTiers'
+
+describe('handle converiting numbers to roman numerals', () => {
+
+    it('returns a roman numeral for a valid number', () => {
+        const rom1 = toRoman(1)
+        expect(rom1).toBe('I')
+    })
+
+    it('returns a question mark if the passed value is unknown', () => {
+        const rom1 = toRoman(0)
+        expect(rom1).toBe('?')
+    })
+
+    it('KNOWN_TIERS is an array of numbers', () => {
+        expect(Array.isArray(KNOWN_TIERS)).toBe(true)
+        let allAreNumbers = KNOWN_TIERS.every(tier => typeof tier === 'number')
+        expect(allAreNumbers).toBe(true)
+    })
+
+})
--- src/utils/tankTiers.tsx
+++ src/utils/tankTiers.tsx
@@ -1,24 +1,17 @@
 
-type RomanTuple = [
-    /** Arabic style number for the tank tier */
-    number,
-    /** Roman style number for the tank tier */
-    string
-]
+const ROMANS = [ 'I', 'II', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII' ] as const
+type RomanNumeral = typeof ROMANS[number]
 
-const ROMAN_LEGEND: RomanTuple[] = [
-    [1, 'I'], [2, 'II'], [3, 'III'], [4, 'IV'], [5, 'V'], [6, 'VI'], [7, 'VII'], [8, 'VIII'], [9, 'IX'], [10, 'X']
-]
+const KNOWN_TIERS = ROMANS
+    .slice(1)
+    .map((_r, indexAsTierNumerical) => indexAsTierNumerical)
 
-function toRoman(x: number): string {
-    return ROMAN_LEGEND.find(([num]) => num === x)?.[1] ?? '?'
-}
-function fromRoman(x: string): number {
-    return ROMAN_LEGEND.find(([_num, str]) => str === x)?.[0] ?? 0
+function toRoman(x: number): RomanNumeral | '?' {
+    return ROMANS[x-1] ?? '?'
 }
 
 export {
     toRoman,
-    fromRoman,
-    ROMAN_LEGEND
+    ROMANS,
+    KNOWN_TIERS
 }
