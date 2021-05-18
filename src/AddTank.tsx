5a7fd0903bc5b75fc40ea541cc38e9b39c062c25
dave-tn <sr@tinynimbus.com>
2021-05-18 13:47:43 +0100

    tidy

--- src/AddTank.tsx
+++ src/AddTank.tsx
@@ -1,10 +1,6 @@
 import { FC, useState } from 'react'
 
-// import { AddTankEditor } from './AddTankEditor'
-import { Tank, TankTypeSlug } from './typesStuff/Tank'
-
-import { ROMAN_LEGEND, toRoman } from './utils/tankTiers'
-import { toType, VEHICLE_TYPES } from './utils/tankTypes'
+import { TankTypeSlug } from './typesStuff/Tank'
 
 import { SelectNation } from './AddTankComponents/SelectNation'
 import { SelectType } from './AddTankComponents/SelectType'
@@ -20,40 +16,9 @@ export const AddTank:FC<{ setShow: React.Dispatch<React.SetStateAction<boolean>>
 
     // Holds the nation slug for the tanks we want to see
     const [ nation, setNation ] = useState('')
-    // Holds disabled types
+    // Filters
     const [ disabledTypes, setDisabledTypes ] = useState<TankTypeSlug[]>([])
-
     const [ disabledTiers, setDisabledTiers ] = useState<number[]>([])
-
-    // const listOfNations = Object.keys(NATIONS)
-    //     .sort()
-    // const [ selectedNation, setSelectedNation ] = useState(listOfNations[0])
-
-    // Available vehicle types for the selected nation
-    // const listOfTypes = VEHICLE_TYPES
-    //     .map(tt => tt[0])
-    //     .filter(typeSlug => !!tanks.find(t => t.info.nation === selectedNation && t.info.type_slug === typeSlug))
-    // const [ selectedType, setSelectedType ] = useState(listOfTypes[0])
-
-    // // Available tiers for the selected nation & selected vehicle type
-    // const listOfTiers = ROMAN_LEGEND
-    //     .map(tup => tup[0])
-    //     .filter(num => !!tanks.find(t => t.info.nation === selectedNation && t.info.type_slug === selectedType && t.info.level === num))
-    // const [ selectedTier, setSelectedTier ] = useState(0)
-
-
-    // const filteredTanks = tanks
-    //     .filter(t => t.info.nation === selectedNation)
-    //     .filter(t => t.info.type_slug === selectedType)
-    //     .filter(t => selectedTier !== undefined && selectedTier !== 0 ? t.info.level === selectedTier : true)
-    //     .sort((a, b) => {
-    //         return a.info.level > b.info.level ? -1 // default sort by tier
-    //         : b.info.level > a.info.level ? 1
-    //         : a.info.user_string > b.info.user_string ? 1 : -1 // same tier so sort alphabetically
-    //     })
-
-    // const [ selectedTank, setSelectedTank ] = useState<string>()
-    // const tank: Tank | undefined = filteredTanks.find(t => t.info.name === selectedTank)
 
     return (
         <div className={styles.mainWrap}>
