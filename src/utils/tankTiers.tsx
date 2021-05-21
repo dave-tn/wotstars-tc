b9fe9cec0358b0b0722e76085d3a138bdb048205
dave-tn <sr@tinynimbus.com>
2021-05-21 15:36:05 +0100

    whoops...

--- src/utils/tankTiers.tsx
+++ src/utils/tankTiers.tsx
@@ -1,5 +1,5 @@
 
-const ROMANS = [ 'I', 'II', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII' ] as const
+const ROMANS = [ 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII' ] as const
 type RomanNumeral = typeof ROMANS[number]
 
 const KNOWN_TIERS = ROMANS
