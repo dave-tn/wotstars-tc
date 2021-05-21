dffc8c15e8554eb90f9846dccce0ab7a593b6b6c
dave-tn <sr@tinynimbus.com>
2021-05-21 15:05:48 +0100

    use graphql to load tank data based on the fingerprints / state

--- src/TanksTable.tsx
+++ src/TanksTable.tsx
@@ -9,12 +9,55 @@ import { FirepowerRow } from './RowComponents/FirepowerRow'
 import { WeaponHandlingRow } from './RowComponents/WeaponHandlingRow'
 import { MovementRow } from './RowComponents/MovementRow'
 
+import { useQuery, gql } from '@apollo/client'
+import { GQLTank } from './AddTankComponents/SelectTankList'
+
+interface TanksVars {
+    fingerprints: string[]
+}
+
+interface GQLData {
+    tanks: GQLTank[]
+}
+
+const GET_TANKS_QUERY = gql`
+    query GetTanksByFingerprint($fingerprints: [String]!) {
+        tanksByFingerprint(fingerprints: $fingerprints) {
+            id
+            user_string
+            nation
+            type_slug
+            turret {
+                user_string
+                gun {
+                    user_string
+                    shot {
+                        user_string
+                        damage
+                        piercing_power
+                        dpm
+                    }
+                }
+            }
+        }
+    }
+`
+
 export const TanksTable:FC<{
     tanks: Tank[]
     showTankEditor: React.Dispatch<React.SetStateAction<boolean>>
 }> = ({ tanks, showTankEditor }) => {
 
-    const [ tankCompData ] = useTankState(tanks)
+    const [ tankCompData, fingerprints ] = useTankState(tanks)
+
+    const { data, loading, error } = useQuery<GQLData, TanksVars>(GET_TANKS_QUERY, {
+        variables: { fingerprints },
+        // onCompleted: ({ tankTiers }) => setAvailableTiers(tankTiers)
+    })
+
+    // TODO: FIXME
+    if (loading) return <div>Loading Tanks Data...</div>
+    if (error) return <div>There was an error loading the tanks data. Maybe try refreshing 🤷‍♂️</div>
 
     return (
         <div className="xxxxxxMain-grid">
