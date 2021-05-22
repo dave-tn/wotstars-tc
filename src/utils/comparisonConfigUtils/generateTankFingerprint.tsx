5d848b3ff5c73a708e5eb1cf0012543776db4444
dave-tn <sr@tinynimbus.com>
2021-05-22 13:24:36 +0100

    update var names

--- src/utils/comparisonConfigUtils/generateTankFingerprint.tsx
+++ src/utils/comparisonConfigUtils/generateTankFingerprint.tsx
@@ -22,12 +22,12 @@ const getUniqueIdString = () => String(uniqueIdGenerator.next().value)
  * @param tankId The tank's WG ID
  * @param xxIndex The chosen tank's index for this particular module/item
  */
-function generateTankFingerprint(tankId: number, chassisIndex: number, engineIndex: number, turretIndex: number, gunIndex: number, ammoIndex: number): Fingerprint {
+function generateTankFingerprint(tankId: number, chassisIndex: number, engineIndex: number, turretIndex: number, gunIndex: number, shotIndex: number): Fingerprint {
 
     // Use a unique ID so our fingerprint is ensured unique, meaning we can then have
     // the same vehicle configuration showing, if someone would want to do that... (maybe 
     // for comparing ammo or whatever)    
-    const tankFingerprint = `${tankId}.${chassisIndex}.${engineIndex}.${turretIndex}.${gunIndex}.${ammoIndex}.${getUniqueIdString()}` as Fingerprint
+    const tankFingerprint = `${tankId}.${chassisIndex}.${engineIndex}.${turretIndex}.${gunIndex}.${shotIndex}.${getUniqueIdString()}` as Fingerprint
     return tankFingerprint
 }
 
